a8c7d9ef2f69243a92493b3adf0e0e2517062a53
Kai Sassnowski <ksassnowski@gmail.com>
2016-05-25 21:18:06 +0200

    add players through game constructor

--- src/Game.php
+++ src/Game.php
@@ -5,14 +5,38 @@ namespace Acme;
 class Game
 {
     /**
+     * @var Player
+     */
+    public $player1;
+
+    /**
+     * @var Player
+     */
+    public $player2;
+
+    /**
+     * Game constructor.
+     *
+     * @param Player $player1
+     * @param Player $player2
+     */
+    public function __construct(Player $player1, Player $player2)
+    {
+        $this->player1 = $player1;
+        $this->player2 = $player2;
+    }
+
+    /**
      * Determines the winner and echoes it.
      *
      * @param Player $player1
      * @param Player $player2
      */
-    public function printWinner(Player $player1, Player $player2)
+    public function printWinner()
     {
-        $winner = $player1->points > $player2->points ? $player1 : $player2;
+        $winner = $this->player1->points > $this->player2->points 
+            ? $this->player1 
+            : $this->player2;
 
         echo "And the winner is {$winner->name}";
     }
--- src/Run.php
+++ src/Run.php
@@ -8,4 +8,6 @@ use Acme\Game;
 $player1 = new Player('Kai', 10);
 $player2 = new Player('Philip', 25);
 
-(new Game())->printWinner($player1, $player2);
+$game = new Game($player1, $player2);
+
+$game->printWinner();
